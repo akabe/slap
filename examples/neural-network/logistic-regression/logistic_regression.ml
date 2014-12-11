739c1a9bd71655a575f972bdae2193f6da27099a
Akinori ABE <abe@kb.ecei.tohoku.ac.jp>
2014-12-11 22:15:54 +0900

    Add an example of logistic regression

---
+++ examples/neural-network/logistic-regression/logistic_regression.ml
@@ -0,0 +1,40 @@
+(* Logistic regression --- A classic single-layer neural network *)
+
+open Format
+open Slap.Io
+open Slap.Size
+open Slap.Common
+open Slap.D
+
+let sigm a = 1.0 /. (1.0 +. exp (~-. a))
+
+let train eta w x t =
+  let y = sigm (dot ~x w) in
+  axpy ~alpha:(eta *. (t -. y)) ~x w
+
+let main () =
+  let input_dim = three in
+  let make_input x1 x2 = Vec.of_list_dyn input_dim [x1; x2; 1.0] in
+  (* training set (a target must be 1.0 or 0.0) *)
+  let samples = [|
+      (make_input 0.0 0.0, 1.0);
+      (make_input 1.0 0.0, 1.0);
+      (make_input 0.0 1.0, 0.0);
+      (make_input 1.0 1.0, 0.0);
+    |] in
+  (* Create a weight vector *)
+  let w = Vec.random input_dim in
+  (* training *)
+  for i = 1 to 1000 do
+    let k = Random.int (Array.length samples) in
+    let (x, t) = samples.(k) in
+    train 0.5 w x t
+  done;
+  (* Show results of training *)
+  printf "w = @[[%a]@]@." pp_rfvec w;
+  Array.iteri (fun i (x, t) ->
+               printf "[%d] x = @[[%a]@], t = %g, y = %g@."
+                      i pp_rfvec x t (sigm (dot ~x w)))
+              samples
+
+let () = main ()
--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -1,5 +1,5 @@
 (* OASIS_START *)
-(* DO NOT EDIT (digest: 267bcd5208ce555dc6340aba98f59911) *)
+(* DO NOT EDIT (digest: f0e3cfc82fd19405b608bab4997f9a6d) *)
 module OASISGettext = struct
 (* # 22 "src/oasis/OASISGettext.ml" *)
 
@@ -714,6 +714,7 @@ let package_default =
           ("examples/porting/lu_factorize", ["lib"]);
           ("examples/porting/k-means", ["lib"]);
           ("examples/neural-network/perceptron", ["lib"]);
+          ("examples/neural-network/logistic-regression", ["lib"]);
           ("examples/mult", ["lib"]);
           ("examples/linsys", ["lib"])
        ]
@@ -724,6 +725,6 @@ let conf = {MyOCamlbuildFindlib.no_automatic_syntax = false}
 
 let dispatch_default = MyOCamlbuildBase.dispatch_default conf package_default;;
 
-# 728 "myocamlbuild.ml"
+# 729 "myocamlbuild.ml"
 (* OASIS_STOP *)
 Ocamlbuild_plugin.dispatch dispatch_default;;
