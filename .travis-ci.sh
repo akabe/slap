90098474d542c817ff46b20cff372046f946b913
Akinori ABE <abe@kb.ecei.tohoku.ac.jp>
2015-07-02 12:10:25 +0900

    Add detailed tests

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,19 +1,30 @@
 # Dependencies
-APT_DEPENDS="ocaml ocaml-native-compilers camlp4-extra ocaml-compiler-libs opam liblapack-dev"
-OPAM_DEPENDS="ocamlfind cppo lacaml ounit"
+# (Lacaml 7.2.5 or below cannot pass tests because they have a bug in the complex version of Vec.ssqr_diff)
+APT_DEPENDS="opam liblapack-dev"
+# OPAM_DEPENDS="ocamlfind cppo lacaml>=7.2.6 ounit"
+OPAM_DEPENDS="ocamlfind cppo ounit"
 
 # Select PPA
 case "$OCAML_VERSION,$OPAM_VERSION" in
+  4.02.0,1.2.0) ppa=avsm/ocaml42+opam12 ;;
   4.02.1,1.2.0) ppa=avsm/ocaml42+opam12 ;;
+  4.02.2,1.2.0) ppa=avsm/ocaml42+opam12 ;;
   *) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;
 esac
 
-# Install OCaml, OPAM and $APT_DEPENDS
+# Install OPAM and $APT_DEPENDS
 echo "yes" | sudo add-apt-repository ppa:$ppa
 sudo apt-get update -qq
 sudo apt-get install -qq ${APT_DEPENDS}
+
+# Install OCaml
 export OPAMYES=1
 export OPAMVERBOSE=1
+opam init
+opam switch $OCAML_VERSION
+eval `opam config env`
+
+# Show OCaml and OPAM versions
 echo OCaml version
 ocaml -version
 echo OPAM versions
@@ -21,12 +32,19 @@ opam --version
 opam --git-version
 
 # Install $OPAM_DEPENDS
-opam init
 opam install ${OPAM_DEPENDS}
-eval `opam config env`
+
+# Install Lacaml
+wget https://github.com/mmottl/lacaml/releases/download/v7.2.6/lacaml-7.2.6.tar.gz
+tar zxvf lacaml-7.2.6.tar.gz
+cd lacaml-7.2.6
+./configure
+make
+make install
+cd ..
 
 # Test
-./configure --enable-tests --enable-examples
+./configure $CONFIG --enable-tests --enable-examples
 make
 make test
 
