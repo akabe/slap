82ab2794a607544b85d857eb0529c790765279c7
Akinori ABE <abe@sf.ecei.tohoku.ac.jp>
2016-04-24 21:57:47 +0900

    Improve macros and file generation

---
+++ lib/slap_version.cppo.ml
@@ -0,0 +1,13 @@
+(** The version number of SLAP. *)
+
+(** The version number of SLAP: ["major.minor.micro"] *)
+let version = STRINGIFY(SLAP_VERSION_STRING)
+
+(** The major version of SLAP. *)
+let major = SLAP_MAJOR
+
+(** The minor version of SLAP. *)
+let minor = SLAP_MINOR
+
+(** The micro version of SLAP. *)
+let micro = SLAP_PATCH
