9ee335de9820fb9013b77d4baf44244ed47186fd
Akinori ABE <abe_akinori@cyberagent.co.jp>
2016-05-27 10:10:49 +0900

    Fix branching and '-e' option in .travis-ci.sh

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -18,15 +18,19 @@ echo y | sh opam_installer.sh $BINDIR $OCAML_VERSION
 eval `opam config env`
 
 # Install OPAM packages
+set +e # Disable '-e' option for branching
 if [ "$OCAML_VERSION" == "4.03.0" ]; then
+    set -e  # Enable '-e'
     # OCaml 4.03.0 possibly requires reinstallation of ocamlbuild. (bug?)
     # [STDERR] Warning: Won't be able to compile a native plugin
     #          Failure: Cannot find "ocamlbuild.cmo" in ocamlbuild -where directory.
     if ! opam install $OPAM_PKGS; then
+        echo "\033[31m[WARN] Retry installation of OPAM packages after rebuilding OCamlbuild.\033[0m"
         opam reinstall ocamlbuild
         opam install $OPAM_PKGS
     fi
 else
+    set -e # Enable '-e'
     opam install $OPAM_PKGS
 fi
 
