b624dad10db3f0f9a16e05944121f0601837911e
Akinori ABE <aabe.65535@gmail.com>
2017-11-29 22:49:33 +0900

    :pencil2: fix e-mail address

--- lib/slap_mat.cppo.ml
+++ lib/slap_mat.cppo.ml
@@ -1,6 +1,6 @@
 (* Sized Linear Algebra Package (SLAP)
 
-  Copyright (C) 2013- Akinori ABE <abe@kb.ecei.tohoku.ac.jp>
+  Copyright (C) 2013- Akinori ABE <aabe.65535@gmail.com>
 
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
